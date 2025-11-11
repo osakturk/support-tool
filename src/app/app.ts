548ede5cefe658273d013cc4340d7fceabaf4d74
Omer Akturk <omer.akturk@rabobank.nl>
2025-11-11 10:04:32 +0100

    refactor: split components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,42 +1,14 @@
 import { Component } from '@angular/core';
+import { Header } from "./header/header";
+import { Status } from "./dashboard/status/status";
+import { Tickets } from "./dashboard/tickets/tickets";
+import { Traffic } from "./dashboard/traffic/traffic";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Header, Status, Traffic, Tickets]
 })
-export class App {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
-}
+export class App {}
