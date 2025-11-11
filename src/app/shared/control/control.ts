11ccc2684b33dc32a028a97ab21ae5b895748ac4
Omer Akturk <omer.akturk@rabobank.nl>
2025-11-11 13:45:28 +0100

    refactor: update css files

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,10 +1,11 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.html',
   styleUrl: './control.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class Control {
   label = input.required<string>()
