4a4614e319dab0a067acd65a49400e777bd32072
Omer Akturk <omer.akturk@rabobank.nl>
2025-11-12 09:44:00 +0100

    feat: add interval with literal types

--- src/app/dashboard/status/status.ts
+++ src/app/dashboard/status/status.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './status.css',
 })
 export class Status {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if(rnd < 0.5){
+        this,this.currentStatus = 'online'
+      } else if (rnd < 0.9){
+        this.currentStatus = 'offline'
+      } else {
+        this.currentStatus = 'unknown'
+      }
+    }, 5000)
+  }
 }
