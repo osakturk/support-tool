45b016b89651663016c5972454007e4e6124ee5b
Omer Akturk <omer.akturk@rabobank.nl>
2025-11-12 11:09:07 +0100

    feat: add template variables

--- src/app/dashboard/new-ticket/new-ticket.ts
+++ src/app/dashboard/new-ticket/new-ticket.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { Button } from "../../shared/button/button";
 import { Control } from "../../shared/control/control";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [Button, Control],
+  imports: [Button, Control, FormsModule],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css',
 })
 export class NewTicket {
-
+  onSubmit(title: HTMLInputElement) {
+    console.dir('ENTERED VALUE'+title.value);
+  }
 }
