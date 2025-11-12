56736db349937d58de7ed42df3f28351597a0345
Omer Akturk <omer.akturk@rabobank.nl>
2025-11-12 09:49:26 +0100

    feat: add on init interface for tiny typo errors

--- src/app/dashboard/status/status.ts
+++ src/app/dashboard/status/status.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-status',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './status.html',
   styleUrl: './status.css',
 })
-export class Status {
+export class Status implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
   constructor() {}
